dea768f8ee8341b511718615d793e95b532eab6e
adhirajnanda <115486777+adhirajnanda@users.noreply.github.com>
2023-06-12 16:24:11 +0530

    commit 2nd  after student reg and login

--- src/components/Cregistration.js
+++ src/components/Cregistration.js
@@ -1,138 +1,209 @@
 import React from "react";
+import { useState } from "react";
 
 function Cregistration() {
+  const [form, setForm] = useState({});
 
+  const handleForm = (e) => {
+    //console.log(e.target.value, e.target.name);
+
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5001/api/company/", {
+      method: "POST",
+
+      body: JSON.stringify(form),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    if (!data.success) {
+      alert("Account Created Successfully", "Success");
+    } else {
+      alert("Invalid Credentials", "danger");
+    }
+  };
 
   return (
-    <form>
-    <div className="wrapper rounded bg-white ms-3 me-3">
-      <div className="h2 mt-4 text-center rf">Registration Form</div>
-      <div className="h5 mt-4 text-center">
-        Employers/Recruiters Registration
-      </div>
-
-      <div className="form">
-        <div className="row">
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>Company Name*</label>
-            <input type="text" className="form-control" required />
-          </div>
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>CIN/Company Identity Number*</label>
-            <input type="text" className="form-control" required />
-          </div>
+    <form onSubmit={handleSubmit}>
+      <div className="wrapper rounded bg-white ms-3 me-3">
+        <div className="h2 mt-4 text-center rf">Registration Form</div>
+        <div className="h5 mt-4 text-center">
+          Employers/Recruiters Registration
         </div>
 
-        <div className="row">
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>Industry Type</label>
-            <input type="text" className="form-control" required />
+        <div className="form">
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>Company Name*</label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="companyname"
+              />
+            </div>
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>CIN/Company Identity Number*</label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="CIN"
+              />
+            </div>
           </div>
-          
-        </div>
-        <div className="row">
-          
-          <div className="form-group shadow-textarea mt-2">
-            <label for="exampleFormControlTextarea6">
-              Company/Firm Description
-            </label>
-            <textarea 
-              className="form-control z-depth-1"
-              id="exampleFormControlTextarea6"
-              rows="3"
-              placeholder="Company description....."
-            ></textarea>
-          </div>
-        </div>
-      </div>
-      <div className="main mb-2">
-        <div className="h5 mt-4 text-center">Address Details</div>
-        <div className="row">
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>Website</label>
-            <input type="text" className="form-control" required />
-          </div>
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>Phone Number</label>
-            <input type="tel" className="form-control" required />
-          </div>
-        </div>
 
-        <div className=" my-md-2 my-3">
-          <label>Country</label>
-          <select id="sub" required>
-            <option value="" selected hidden>
-              Choose Option
-            </option>
-            <option value="India">India</option>
-            <option value="Usa">Usa</option>
-            <option value="Nepal">Nepal</option>
-            <option value="England">England</option>
-            <option value="Japan">Japan</option>
-          </select>
-        </div>
-        <div className="row">
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>State*</label>
-            <input type="text" className="form-control" required />
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>Industry Type</label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="industrytype"
+              />
+            </div>
           </div>
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>City*</label>
-            <input type="text" className="form-control" required />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-6 mt-md-0 mt-3">
-            <div className="form-group shadow-textarea">
-              <label for="exampleFormControlTextarea6">Address</label>
+          <div className="row">
+            <div className="form-group shadow-textarea mt-2">
+              <label for="exampleFormControlTextarea6">
+                Company/Firm Description
+              </label>
               <textarea
                 className="form-control z-depth-1"
                 id="exampleFormControlTextarea6"
                 rows="3"
-                placeholder="Address"
+                placeholder="Company description....."
+                onChange={handleForm}
+                name="description"
               ></textarea>
             </div>
           </div>
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>Locality*</label>
-            <input type="text" className="form-control" required />
-          </div>
         </div>
-
-        <div className="h5 mt-4 text-center">Contact Details</div>
-        <div className="row">
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>Name (person1)*</label>
-            <input type="text" className="form-control" required />
-          </div>
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>Email (person1)*</label>
-            <input type="text" className="form-control" required />
-          </div>
-        </div>
-        <div className="row">
-         
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>Mobile (person2)*</label>
-            <input type="text" className="form-control" required />
-          </div>
-          <div className="col-md-6 mt-md-0 mt-3">
-            <label>Password*</label>
-            <input type="password" className="form-control" required />
-            <span>(Minimum 5 characters are required)</span>
+        <div className="main mb-2">
+          <div className="h5 mt-4 text-center">Address Details</div>
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>Website</label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="website"
+              />
+            </div>
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>Phone Number</label>
+              <input
+                type="tel"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="phone"
+              />
+            </div>
           </div>
 
-        </div>
-        
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>Country*</label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="country"
+              />
+            </div>
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>State*</label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="state"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+              <div className="form-group shadow-textarea">
+                <label for="exampleFormControlTextarea6">Address</label>
+                <textarea
+                  className="form-control z-depth-1"
+                  id="exampleFormControlTextarea"
+                  rows="3"
+                  placeholder="Address"
+                  onChange={handleForm}
+                  name="address"
+                ></textarea>
+              </div>
+            </div>
+          </div>
 
-        <div className="text-center">
-          <button type="button" className="btn btn-primary mt-3">
-            Complete Registration
-          </button>
-          
+          <div className="h5 mt-4 text-center">Contact Details</div>
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>Name </label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="name"
+              />
+            </div>
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>Email </label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="email"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>Mobile </label>
+              <input
+                type="text"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="mobile"
+              />
+            </div>
+            <div className="col-md-6 mt-md-0 mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                required
+                onChange={handleForm}
+                name="password"
+              />
+              <span>(Minimum 5 characters are required)</span>
+            </div>
+          </div>
+
+          <div>
+            <input className="btn btn-primary mt-3" type="submit" />
+          </div>
         </div>
       </div>
-    </div>
     </form>
   );
 }
